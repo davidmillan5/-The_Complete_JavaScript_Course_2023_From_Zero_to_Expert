5212051febe32f937313d2e01ae3461a32fb595e
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-04-07 08:01:05 -0500

    Function Lessons Completed, pending on the Coding Challenge

---
+++ Section 3 - JavaScript Fundamentals Part II/6 - Functions Calling Other Functions/starter/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let fruitPieces = 4;
+
+function cutFruitPieces(fruit) {
+  return fruit * fruitPieces;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
